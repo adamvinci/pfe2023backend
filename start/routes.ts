b6b1c9c8b5fdb123750d356e80244520a52649ff
adam <adame.barhdadi@student.vinci.be>
2023-12-05 11:21:54 +0100

    test pipeline

--- start/routes.ts
+++ start/routes.ts
@@ -23,5 +23,5 @@ import Database from '@ioc:Adonis/Lucid/Database'
 
 Route.get('/', async () => {
   const products = await Database.from('product').select('*');
-  return { hello: 'test', products }
+  return { hello: 'hello', products }
 })
